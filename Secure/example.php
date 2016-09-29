12ace0b583d4ec5ce885e4ff55b9a1d1402b7b5b
Kirill Kholodilin <kirill.kholodilin@hitmeister.de>
2016-09-29 11:22:23 +0200

    * Make code PSR-2 Standard compilant

--- Secure/example.php
+++ Secure/example.php
@@ -1,7 +1,7 @@
 <?php
 
-	use ArcheeNic\HackBrainTime\Secure;
+use ArcheeNic\HackBrainTime\Secure;
 
-	/** @TODO: Suppress implicit require by using autoloader */
-	include_once(__DIR__.'/secure.php');
-	new Secure('post,files');
+/** @TODO: Suppress implicit require by using autoloader */
+include_once(__DIR__.'/secure.php');
+new Secure('post,files');
